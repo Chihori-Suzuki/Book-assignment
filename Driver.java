20ed2177c81138e2578992ec4f14fae4185650bb
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-10-21 10:47:54 -0700

    first commit

--- Driver.java
+++ Driver.java
@@ -23,20 +23,13 @@ public class Driver {
 		studentArray[0] = new Students(null, "");
 		studentArray[1] = new Students("Jane", "Dew");
 		
-//		Students st1 = new Students("", "");
-//		Students st2 = new Students("Jane", "Dew");
-//		System.out.println(st1.getFirstName() + " " + st1.getLastName());
-//		System.out.println(st2.getFirstName() + " " + st2.getLastName());
-		
 //		printStudents(studentArray);
 		studentArray[0].setFirstName("John");
 		studentArray[0].setLastName("Doe");
-//		st1.setFirstName("John");
-//		st1.setLastName("Doe");
+
 		
 		System.out.println("\nPrinting the student after the update");
-//		System.out.println(st1.getFirstName() + " " + st1.getLastName());
-//		System.out.println(st2.getFirstName() + " " + st2.getLastName());
+
 		
 		Book[] bookArray = new Book[1];
 		
